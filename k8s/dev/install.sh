17e0e5463cdf7c6eda1560f046167194398d3b18
Johnlinmadan <772181827@qq.com>
2017-01-11 21:15:39 +0800

    添加k8s自动部署功能

--- k8s/dev/install.sh
+++ k8s/dev/install.sh
@@ -1,1 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
+kubectl get svc | grep -q data-collect
+if [ "$?" == "1" ];then
+	kubectl create -f data_collect-service.yaml --record
+	kubectl get svc | grep -q data-collect
+	if [ "$?" == "0" ];then
+		echo "data_collect-service install success!"
+	else
+		echo "data_collect-service install fail!"
+	fi
+else
+	echo "data_collect-service is exist!"
+fi
+kubectl get pods | grep -q data-collect
+if [ "$?" == "1" ];then
+	kubectl create -f data_collect-deployment.yaml --record
+	kubectl get pods | grep -q data-collect
+	if [ "$?" == "0" ];then
+		echo "data_collect-deployment install success!"
+	else
+		echo "data_collect-deployment install fail!"
+	fi
+else
+	kubectl delete -f data_collect-deployment.yaml
+	kubectl get pods | grep -q data-collect
+	while ( "$?" == "1" )
+	do
+	kubectl create -f data_collect-deployment.yaml --record
+	kubectl get pods | grep -q data-collect
+	if [ "$?" == "0" ];then
+		echo "data_collect-deployment update success!"
+	else
+		echo "data_collect-deployment update fail!"
+	fi
+	done
+fi
